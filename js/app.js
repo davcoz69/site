09247e8b08f93f95f92724b2379ed7db947cd928
davcoz69 <davcoz69@gmail.com>
2022-04-26 11:01:05 +0200

    news

--- js/app.js
+++ js/app.js
@@ -19,5 +19,21 @@ const navSlide = () => {
 
     });
 }
+const goTopBtn = document.querySelector('.go-top-btn');
 
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight() {
+    if (window.scrollY > 200) {
+        goTopBtn.style.display = "flex"
+    } else {
+        goTopBtn.style.display = "none"
+    }
+}
+goTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
 navSlide();
